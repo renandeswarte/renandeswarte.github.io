f9abb0773da9ff648daba1a8ed770c7efa490490
Renan Deswarte <renandeswarte@gmail.com>
2015-10-06 22:43:45 -0700

    add project links style

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(function(){
 	$.stellar();
+
+  // Expand the projects / Github links
+  $('.project-link').on('mouseenter', function() {
+    $(this).addClass('expand');
+  }).on('mouseleave', function() {
+    $(this).removeClass('expand');
+  });
+  
 })
