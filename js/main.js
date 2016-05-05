8afa5f136a58c60fa1e8793cb245597f43953768
Renan Deswarte <renandeswarte@gmail.com>
2016-05-04 23:35:14 -0700

    cleanup: remove unused js files for parallax

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
 $(function(){
-	$.stellar();
 
-  // Expand the projects / Github links
-  $('.project-link').on('mouseenter', function() {
-    $(this).addClass('expand');
-  }).on('mouseleave', function() {
-    $(this).removeClass('expand');
-  });
-  
 })
